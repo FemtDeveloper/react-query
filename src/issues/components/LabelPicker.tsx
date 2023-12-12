8cefc83fac9d437f44ca2e026fc8bcb427c9ccaa
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2023-12-11 20:30:04 -0500

    git issues

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,14 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+import { useLabels } from "../hooks/useLabels";
+import { LoadingIcon } from "../../shared/LoadingIcon";
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onLabelChaged: (label: string) => void;
+}
+
+export const LabelPicker = ({ selectedLabels, onLabelChaged }: Props) => {
+  const { labelQuery } = useLabels();
+
+  if (labelQuery.isLoading) return <LoadingIcon />;
+
   return (
     <div>
-        <span 
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #ffccd3`, color: '#ffccd3' }}
+      {labelQuery.data?.map((label) => (
+        <span
+          key={label.id}
+          className={`badge rounded-pill m-1 label-picker ${
+            selectedLabels.includes(label.name) ? "label-active" : null
+          }`}
+          style={{
+            border: `1px solid #${label.color}`,
+            color: `#${label.color}`,
+          }}
+          onClick={() => onLabelChaged(label.name)}
         >
-            Primary
+          {label.name}
         </span>
-        
+      ))}
     </div>
-  )
-}
+  );
+};
---
+++ src/issues/hooks/useLabels.tsx
@@ -0,0 +1,37 @@
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { Label } from "../interfaces/label";
+import { sleep } from "../../api/helpers/time";
+
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Label[]>("/labels");
+  console.log({ data });
+  return data;
+};
+
+export const useLabels = () => {
+  const labelQuery = useQuery(["labels"], getLabels, {
+    staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 791921801,
+        node_id: "MDU6TGFiZWw3OTE5MjE4MDE=",
+        url: "https://api.github.com/repos/facebook/react/labels/%E2%9D%A4%EF%B8%8F",
+        name: "❤️",
+        color: "ffffff",
+        default: false,
+      },
+      {
+        id: 180616330,
+        node_id: "MDU6TGFiZWwxODA2MTYzMzA=",
+        url: "https://api.github.com/repos/facebook/react/labels/Component:%20Optimizing%20Compiler",
+        name: "Component: Optimizing Compiler",
+        color: "bfdadc",
+        default: false,
+      },
+    ],
+  });
+
+  return { labelQuery };
+};
