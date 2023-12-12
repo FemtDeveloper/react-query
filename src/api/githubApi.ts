6e0e7415358fa68b1bebebb5fd60dc6d7faa8046
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2023-12-11 23:08:08 -0500

    .env added

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 export const githubApi = axios.create({
   baseURL: "https://api.github.com/repos/facebook/react",
   headers: {
-    Authorization:
-      "Bearer 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",
+    Authorization: `Bearer ${import.meta.env.VITE_GITHUB_API_KEY}`,
   },
 });
